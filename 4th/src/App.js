aa4a492d46b7afe48a577118a905a35579dfee8f
Suyash  Sutar <elohim2199@gmail.com>
2020-08-17 13:35:33 +0530

    Added 4th react app

---
+++ 4th/src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.brand = "ferrari";
+        this.state = {color:'red', ty:'2-seater'}
+    }
+    present() {
+        return this.state.color + ' coloured and ' + this.state.ty ;
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hi</h1>
+                <p>I have a {this.brand} which is {this.present()} </p>
+            </div>
+        );
+    }
+}
+
+export default App;
